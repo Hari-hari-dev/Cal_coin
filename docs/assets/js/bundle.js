d4b8320807ae40557f606da79b97315c96beed0c
HurryDev <hari-hari-dev@proton.me>
2025-04-11 15:12:29 -0700

    page.js change 11

--- docs/assets/js/bundle.js
+++ docs/assets/js/bundle.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.anchor = {}));
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.solanaWeb3 = {}));
 })(this, (function (exports) { 'use strict';
 
   // src/codes.ts
