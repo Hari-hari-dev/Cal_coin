700bca06ab9c58dfc0cd4758e7bdf3fd63d64f6d
HurryDev <hari-hari-dev@proton.me>
2025-06-06 14:20:27 -0700

    ok, got synching, init script made but lib.rs needs staking

--- docs/assets/js/borsh-shim.js
+++
@@ -1,4 +0,0 @@
-import borsh from 'borsh-original';  // import the original borsh using the alias
-export const serialize = borsh.serialize;
-export const deserialize = borsh.deserialize;
-export const deserializeUnchecked = borsh.deserializeUnchecked;
